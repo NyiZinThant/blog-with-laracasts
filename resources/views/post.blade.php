e177000fe230ac47701cc68ee08827af251a96bf
NyiZinThant <codebynyi@gmail.com>
2022-06-10 09:03:35 +0630

    ep-7

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil itaque vel et iste, voluptatem dignissimos, possimus eius enim maxime quia velit voluptate! Deleniti odit obcaecati culpa cum laboriosam fugiat pariatur.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione adipisci, animi error sed dolorum veritatis ducimus atque aspernatur laborum deleniti hic? Modi error vitae magni est blanditiis. Cum, numquam natus!
+        </p>
+    </article>
+    <a href="/">back</a> 
+</body>
