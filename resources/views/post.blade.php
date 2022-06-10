abbb9d187a39506c4a2eae31c1a4a26b1d954fb6
NyiZinThant <codebynyi@gmail.com>
2022-06-10 11:51:59 +0630

    ep-8,9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,11 +4,6 @@
     <link rel="stylesheet" href="/style.css">
 </head>
 <body>
-    <article>
-        <h1><a href="/post">My First Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil itaque vel et iste, voluptatem dignissimos, possimus eius enim maxime quia velit voluptate! Deleniti odit obcaecati culpa cum laboriosam fugiat pariatur.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione adipisci, animi error sed dolorum veritatis ducimus atque aspernatur laborum deleniti hic? Modi error vitae magni est blanditiis. Cum, numquam natus!
-        </p>
-    </article>
+    {!! $post !!}
     <a href="/">back</a> 
 </body>
