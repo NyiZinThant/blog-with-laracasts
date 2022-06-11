942120ed2b78c7a8fc8e3b054fded94d621ac479
NyiZinThant <codebynyi@gmail.com>
2022-06-11 07:57:05 +0630

    ep-11

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,22 +4,9 @@
     <link rel="stylesheet" href="/style.css">
 </head>
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil itaque vel et iste, voluptatem dignissimos, possimus eius enim maxime quia velit voluptate! Deleniti odit obcaecati culpa cum laboriosam fugiat pariatur.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione adipisci, animi error sed dolorum veritatis ducimus atque aspernatur laborum deleniti hic? Modi error vitae magni est blanditiis. Cum, numquam natus!
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil itaque vel et iste, voluptatem dignissimos, possimus eius enim maxime quia velit voluptate! Deleniti odit obcaecati culpa cum laboriosam fugiat pariatur.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione adipisci, animi error sed dolorum veritatis ducimus atque aspernatur laborum deleniti hic? Modi error vitae magni est blanditiis. Cum, numquam natus!
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil itaque vel et iste, voluptatem dignissimos, possimus eius enim maxime quia velit voluptate! Deleniti odit obcaecati culpa cum laboriosam fugiat pariatur.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione adipisci, animi error sed dolorum veritatis ducimus atque aspernatur laborum deleniti hic? Modi error vitae magni est blanditiis. Cum, numquam natus!
-        </p>
-    </article>
+    <?php foreach ($posts as $post): ?>
+        <article>
+            <?= $post ?>
+        </article> 
+    <?php endforeach; ?>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,20 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    $posts = Post::all();
+    return view('posts', ["posts" => $posts]);
 });
 
 Route::get("/posts/{post}", function ($slug) {
-    $path = __DIR__ . "/../resources/views/posts/{$slug}.html";
-    
-    if (! file_exists($path)) {
-        abort(404);
-    }
-
-    $post = file_get_contents($path);
-
-    return view("post",[
-        "post" => $post,
-    ]);
+    dd(Post::find($slug));
+    return view("post", ["post" => Post::find($slug)]);
 
 })->where("post", "[A-z_\-]+");
