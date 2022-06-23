e9b09525996aceb80a5c9048ddb6f1bea6dbb80f
NyiZinThant <codebynyi@gmail.com>
2022-06-23 09:04:02 +0630

    ep-42,43

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -11,7 +11,8 @@
         All
     </x-dropdown-item>
     @foreach ($categories as $category)
-        <x-dropdown-item href="/?category={{ $category->slug }}" {{-- :active="isset($currentCategory) && $currentCategory->is($category)" --}} {{-- Alt method --}}
+        <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}" 
+            {{-- :active="isset($currentCategory) && $currentCategory->is($category)" --}} {{-- Alt method --}}
             :active='request()->is("categories/{$category->slug}")'>
             {{ ucwords($category->name) }}
         </x-dropdown-item>
